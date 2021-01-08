165f9d5742ccac1a117d3938eaa00355ccb53a34
Mouad Kerdoud <mkerdoud2@gmail.com>
2021-01-09 00:38:06 +0100

    Designed the events page for the admin profile

--- src/components/admin/Events/Events.js
+++ src/components/admin/Events/Events.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Sidebar from "../../layout/Sidebar/Sidebar"
+import Table from "../../layout/Table/Table"
 
 const Events = () => {
     return (
         <div className="container" >
-        <Sidebar />
-        <div className="content">
-            <h1>THIS IS THE EVENTS</h1>
+            <Sidebar />
+            <div className="content">
+                <h1 className="page-title">Events</h1>
+                <button className="add-new">Add New</button>
+                <Table className="table" />
+            </div >
         </div>
-    </div>
     )
 }
 
