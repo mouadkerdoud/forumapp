4d60245a6d1a35a5643a16a4de428045235fefc9
Mouad Kerdoud <mkerdoud2@gmail.com>
2021-01-19 18:28:23 +0100

    Adding of the Admin Service Class and the implementation of the Admin related methods

---
+++ src/services/admin.service.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import UserService from "./user.service"
+
+
+const API_URL = 'http://192.168.1.13:8080/api/admin';
+const headers={}
+
+class AdminService{
+    constructor(){
+        UserService.currentUser.subscribe(data=>{
+            this.headers = {
+                "Content-Type": "application/json",
+                "authorization": "Bearer " + (data ? data.token: "")
+            }
+        })
+    }
+
+    updateUser(user){
+        return axios.put(API_URL + "updateUser", JSON.stringify(user), {headers: this.headers})
+    }
+
+    deleteUser(userId){
+        return axios.post(API_URL + userId, {headers: this.headers})
+    }
+
+    findAllUsers(){
+        return axios.get(API_URL + "users", {headers: this.headers})
+    }
+
+}
+
+export default new AdminService();
